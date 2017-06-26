2edbd1191230153d4de13e34e5602ed7ae8e62fa
Katharina Dannenberg <skattkista@outlook.de>
2017-06-26 17:27:31 +0200

    some more testing of the ida function

--- R/ida_example_from_doc.R
+++ R/ida_example_from_doc.R
@@ -1,9 +1,12 @@
 library(pcalg)
 ## Simulate the true DAG
-set.seed(123)
-set.seed(12)
+set.seed(123) # default
+# set.seed(12) # used below
+# set.seed(14) # interesting
+# set.seed(16) # ungerichtete Kanten!
+set.seed(17)
 p <- 7
-# p <- 2
+# p <- 4
 myDAG <- pcalg::randomDAG(p, prob = 0.2) ## true DAG
 myCPDAG <- dag2cpdag(myDAG) ## true CPDAG
 covTrue <- trueCov(myDAG) ## true covariance matrix
@@ -37,13 +40,14 @@ if(require(Rgraphviz)) {
 
 check_formula <- function(x, y, par_x) {
   cat("IDA:")
-  cat(ida(x,y,cov.d,pc.fit@graph))
+  ida_value <- ida(x,y,cov.d,pc.fit@graph)
+  cat(ida_value)
   
   cat("\n")
   
   if (!missing(par_x)) {
     cat("Formel:")
-    formel_value <- solve(C[x, x], C[x, y, drop = FALSE])[1, ]
+    formel_value <- solve(cov.d[par_x, par_x], cov.d[par_x, y, drop = FALSE])[1, ]
     cat(formel_value)
   } else {
     cat("Formel:")
@@ -54,15 +58,51 @@ check_formula <- function(x, y, par_x) {
   cat("\n")
   
   cat("equal?: ")
-  cat(as.numeric(ida(x,y,cov.d,pc.fit@graph)) == formel_value)
+  cat(abs(as.numeric(ida_value) - formel_value) < 1e-10)
   cat("\n")
 }
 
-for (x in c(1,2,3,5,6)) {  # immer TRUE
-  for (y in seq(1:7)) {
-    print(paste0("x = ", x, ", y = ", y))
-    check_formula(x,y)
-  }
+# Beispiel set.seed(12)
+# for (x in c(1,2,3,5,6)) {  ohne Eltern 
+#   for (y in seq(1:7)) {
+#     print(paste0("x = ", x, ", y = ", y))
+#     check_formula(x,y)
+#   }
+# }
+# immer TRUE
+
+for (y in 1:7) {     # immer TRUE
+  check_formula(4,y,c(4,2,5))
 }
 
+# for (y in 1:7) {      # immer TRUE
+#   check_formula(7,y,c(7,2,6))
+# }
+# Ende des Beispiels
 
+# Beispiel set.seed(2)
+# for (x in c(1)) {  # ohne Eltern 
+#   for (y in seq(1:7)) {
+#     print(paste0("x = ", x, ", y = ", y))
+#     check_formula(x,y)
+#   }
+# }
+# immer TRUE
+
+# for (y in 1:7) {     # immer TRUE
+#   print(paste0("x = ", 6, ", y = ", y))
+#   check_formula(6,y,c(6,4,5))
+# }
+
+# for (y in 1:7) {     # immer TRUE
+#   print(paste0("x = ", 7, ", y = ", y))
+#   check_formula(7,y,c(7,6,3))
+# }
+
+# for (y in 1:7) {     # NICHT immer TRUE (wegen eventueller Eltern (pa2))
+#   print(paste0("x = ", 2, ", y = ", y))
+#   check_formula(2,y,c(5))
+# }
+# Ende des Beispiels
+
+# causalEffect(pc.fit@graph, 1,2)
