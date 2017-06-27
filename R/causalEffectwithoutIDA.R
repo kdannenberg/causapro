ae54eb3f9a589cb511bda8698a5bbdb2dee3d530
Katharina Dannenberg <skattkista@outlook.de>
2017-06-27 11:45:24 +0200

    ida and causalGraph comparable

--- R/causalEffectwithoutIDA.R
+++ R/causalEffectwithoutIDA.R
@@ -13,18 +13,25 @@ par(mfrow = c(3,3))
 pDAG <- pc.fit@graph
 plot(pDAG)  # Warum sind 3 und 4 hier andersrum??
 
-allDAGS <- pdag2allDags(wgtMatrix(pDAG))$dags
+pseudo_ida_by_causalEffect(x = 2, y = 7, graphEst = pDAG, mcov = cov.d)
 
-for (i in 1:dim(allDAGS)[1]) {
-  m <- matrix(allDAGS[i,],p,p, byrow = TRUE)
+
+# only the parameters x, y and graphEst are used;  rest is only to mathc function definition of ida()
+pseudo_ida_by_causalEffect <- function(x, y, mcov, graphEst, method = "", y.notparent = FALSE, verbose = FALSE, all.dags = NA) {
+  allDAGS <- pdag2allDags(wgtMatrix(pDAG))$dags
   
-  #Why is it necessary to transpose m?!
-  DAG_as <-  as(t(m), "graphNEL")
-  plot(DAG_as)
-  
-  # #maybe better: ftM2graphNEL???
-  # source: https://support.bioconductor.org/p/90421/
-  # DAG_ftM2 <- ftM2graphNEL()
-  # plot(DAG)
+  for (i in 1:dim(allDAGS)[1]) {
+    m <- matrix(allDAGS[i,],p,p, byrow = TRUE)
+    
+    #Why is it necessary to transpose m?!
+    DAG_as <-  as(t(m), "graphNEL")
+    plot(DAG_as)
+    
+    print(causalEffect(DAG_as, x = x, y = y))
+    # #maybe better: ftM2graphNEL???
+    # source: https://support.bioconductor.org/p/90421/
+    # DAG_ftM2 <- ftM2graphNEL()
+    # plot(DAG)
+  }
+  cat(paste("ida:", ida(x.pos = x, y.pos = y, graphEst = pDAG, mcov = mcov)))
 }
-
