cc8374d93b6f14c9c5864ab09fab67c22d05a77e
Marcel Wienoebst <marcel7698@gmx.de>
2017-07-07 13:14:53 +0200

    Improved code for compare function

--- R/functions_conversions.R
+++ R/functions_conversions.R
@@ -6,38 +6,29 @@
 # library(pcalg)
 # library(graph)
 
+# returns adjacency matrix of g with 1s indicating an edge. Edge weights are ignored
+get_adjmatrix <- function(g) {
+  ## list of nodes
+  ln <- names(nodeData(g))
+  ## list of edges in format "a|b"
+  le <- names(edgeData(g))
+  ## construct adjacency matrix
+  n <- length(ln)
+  am <- matrix(0, nrow = n, ncol = n)
+  ## go through all edges and mark them in adjacency matrix
+  for(edge in le) {
+    splt <- strsplit(edge, "\\|")
+    ## this takes linear time and could be replaced by hashmaps
+    from <- which(splt[[1]][1] == ln)[[1]]
+    to <- which(splt[[1]][2] == ln)[[1]]
+    am[[from, to]] <- 1
+  }
+  return(am)
+}
+
+# checks if two graphs g1 and g2 are identical
 compare_graphs <-function(g1, g2) {
-    ## list of nodes
-    ln1 <- names(nodeData(g1))
-    ## list of edges in format "a|b"
-    le1 <- names(edgeData(g1))
-    ## construct adjacency matrix
-    n1 <- length(ln1)
-    am1 <- matrix(0, nrow = n1, ncol = n1)
-    ## go through all edges and mark them in adjacency matrix
-    for(edge in le1) {
-        splt <- strsplit(edge, "\\|")
-        ## this takes linear time and could be replaced by hashmaps
-        from <- which(splt[[1]][1] == ln1)[[1]]
-        to <- which(splt[[1]][2] == ln1)[[1]]
-        am1[[from, to]] <- 1
-    }
-    ## list of nodes
-    ln2 <- names(nodeData(g2))
-    ## list of edges in format "a|b"
-    le2 <- names(edgeData(g2))
-    ## construct adjacency matrix
-    n2 <- length(ln2)
-    am2 <- matrix(0, nrow = n2, ncol = n2)
-    ## go through all edges and mark them in adjacency matrix
-    for(edge in le2) {
-        splt <- strsplit(edge, "\\|")
-        ## this takes linear time and could be replaced by hashmaps
-        from <- which(splt[[1]][1] == ln2)[[1]]
-        to <- which(splt[[1]][2] == ln2)[[1]]
-        am2[[from, to]] <- 1
-    }
-    return(identical(am1,am2))
+    return(identical(get_adjmatrix(g1),get_adjmatrix(g2)))
 }
 
 compare_all_graphs <- function(list_of_graphs) {
