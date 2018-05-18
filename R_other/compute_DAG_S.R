ec01e613bcf0f34e05cb12c8954e05257454af47
Katharina Dannenberg <dannenberg@tcs.uni-luebeck.de>
2018-05-18 15:48:38 +0200

    ...

--- R_other/compute_DAG_S.R
+++ R_other/compute_DAG_S.R
@@ -12,17 +12,19 @@ source("configuration_data.R")
 ## TODO
 
 # debug(protein_causality)
+# debug(plot_graph)
 # debug(analyse_set_of_graphs)
 # debug(determine_set_of_graphs)
 # debug(pymol_mean_effects)
 # debug(compute_if_not_existent)
 
+# debug("tune_alpha_bnlearn")
 
 # debug(estimate_DAG_from_numerical_data)
 
 # undebug(pseudo_ida_by_causalEffect)
 # debug(causal_effects_ida)
-debug(node_colors_pie)
+# debug(node_colors_pie)
 # debug(idaFast)
 # debug(solve)
 # debug(scale_effects)
@@ -36,8 +38,9 @@ debug(node_colors_pie)
 # par(mfrow = c(1,4))
 results_S <- protein_causality_S(#alpha = 0.25, min_pos_var = 0, #data_set = "bin_approx",
                                  #alpha = 0.08, min_pos_var = 0.01,
-                                 alpha = 0.1, min_pos_var = 0.01, cor_cov_FUN = "", # cor_cov_FUN = "none",
-                                 show_variance_cutoff_plot = TRUE, max_conflict_edges = 15,
+                                 alpha = 0.2, min_pos_var = 0.01, cor_cov_FUN = "", # cor_cov_FUN = "none",
+                                 ranked = TRUE, plot_no_isolated_nodes = TRUE,
+                                 show_variance_cutoff_plot = FALSE, max_conflict_edges = 15,
                                  causal_analysis = TRUE, pc_maj_rule = TRUE, pc_solve_confl = TRUE,
                                  for_combined_plot = TRUE, data_in_results = TRUE, plot_with_graphviz = TRUE,
                                  #plot_clusters = TRUE,
